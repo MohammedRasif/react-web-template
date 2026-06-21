f42ae86504242411ddc4d3d24a224249f6bcb62a
MohammedRasif <mohammed660012@gmail.com>
2026-06-21 16:00:50 +0600

    udpate typescript format

---
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+
+interface SidebarProps {
+    isSidebarOpen: boolean;
+    setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    isMobile: boolean;
+}
+
+const UserDashboardSidebar = (_props: SidebarProps) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UserDashboardSidebar;
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import router from './app/router';
 
 
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <React.StrictMode>
     <RouterProvider router={router  } />
